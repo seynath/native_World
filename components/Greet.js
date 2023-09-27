e41501c34c003ae61e5b78ff12051ad1834f8dd3
seynath <117857144+seynath@users.noreply.github.com>
2023-09-27 19:18:53 +0530

    greet update

--- components/Greet.js
+++ components/Greet.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Greet = () => {
   return (
+<View>
+<Text>Hello Greet</Text>
       <Text>Hello Greet</Text>
+      <Text>Hello Greet</Text>
+      <Text>Hello Greet</Text>
+</View>
   )
 }
 
